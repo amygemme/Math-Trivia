6f6313e109ce0da6a7bcb06794d808be30dba19f
amygemme81 <domathlikearockstar@gmail.com>
2019-03-05 21:54:25 -0800

    Currently the display and timer are functioning.  The js is testing for the correct answers and the number of correct and incorrect answers are being logged correctly. I still need to display the score at the end of the game

---
+++ script.js
@@ -0,0 +1,81 @@
+var n = 20;
+var correct = 0;
+var incorrect = 0;
+
+$("#button").on("click", start);
+
+function start() {
+
+   setTimeout(countDown, 1000);
+   $(".container").show();
+   // $(".radio-inline").show();
+}
+function countDown() {
+   n--;
+   if (n > 0) {
+      setTimeout(countDown, 1000);
+   }
+   else {
+      alert("Time Up!")
+      timeup();
+   }
+   console.log(n);
+   $("#count").text(n);
+
+}
+
+function timeup() {
+
+if ($("#opt2a").is(":checked")) {
+   correct++;
+   console.log("you got it")
+ }
+ else{
+//  else if ($("#opt1a").is(":checked") || $("#opt3a").is(":checked")|| $("#opt4a").is(":checked")) {
+    console.log("no go");
+    incorrect++;
+ }
+
+ if ($("#opt4b").is(":checked")) {
+   correct++;
+   console.log("you got it")
+ }
+ else{
+//  else if ($("#opt1a").is(":checked") || $("#opt3a").is(":checked")|| $("#opt4a").is(":checked")) {
+    console.log("no go");
+    incorrect++;
+ }
+
+ if ($("#opt3c").is(":checked")) {
+   correct++;
+   console.log("you got it")
+ }
+ else{
+//  else if ($("#opt1a").is(":checked") || $("#opt3a").is(":checked")|| $("#opt4a").is(":checked")) {
+    console.log("no go");
+    incorrect++;
+ }
+
+ if ($("#opt1d").is(":checked")) {
+   correct++;
+   console.log("you got it")
+ }
+ else{
+//  else if ($("#opt1a").is(":checked") || $("#opt3a").is(":checked")|| $("#opt4a").is(":checked")) {
+    console.log("no go");
+    incorrect++;
+ }
+
+$(".container").hide();
+$("#score").show();
+console.log(correct);
+console.log(incorrect);
+} // close time up function 
+ 
+
+
+
+
+
+
+
